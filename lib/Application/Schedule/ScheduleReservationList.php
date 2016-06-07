2026518c8d6cca5da6a95e5a2dc153a3d6e92f88
Tuarisa <Tuarisa@gmail.com>
2016-06-08 00:16:29 +0200

    herpderp (redux)

--- lib/Application/Schedule/ScheduleReservationList.php
+++ lib/Application/Schedule/ScheduleReservationList.php
@@ -128,7 +128,7 @@ class ScheduleReservationList implements IScheduleReservationList
 				if ($this->ItemEndsOnFutureDate($item))
 				{
 					$indexindays = count($this->_layoutItems);
-					$comparedates = $Item->EndDate() ->GetDifference($Item->BeginDate())->Days();
+					$comparedates = $item->EndDate() ->GetDifference($item->BeginDate())->Days();
 					$spandelta = $indexindays * $comparedates;
 					$endTime = $item->EndDate()->ToTimezone($this->_destinationTimezone);
 					//$endTime = $this->_layoutDateEnd;
