b1545341073f0460b5594ea2b71694fd446e27b0
Tuarisa <Tuarisa@gmail.com>
2016-06-08 00:15:15 +0200

    epic

--- lib/Application/Schedule/ScheduleReservationList.php
+++ lib/Application/Schedule/ScheduleReservationList.php
@@ -128,7 +128,7 @@ class ScheduleReservationList implements IScheduleReservationList
 				if ($this->ItemEndsOnFutureDate($item))
 				{
 					$indexindays = count($this->_layoutItems);
-					$comparedates = $layoutItem->EndDate() ->GetDifference($layoutItem->BeginDate())->Minutes();
+					$comparedates = $Item->EndDate() ->GetDifference($Item->BeginDate())->Days();
 					$spandelta = $indexindays * $comparedates;
 					$endTime = $item->EndDate()->ToTimezone($this->_destinationTimezone);
 					//$endTime = $this->_layoutDateEnd;
