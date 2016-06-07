2b56f3d3dafac60e290bf6f52592d88fc6845c0e
Tuarisa <Tuarisa@gmail.com>
2016-06-08 00:39:20 +0200

    oops, forgot to add the file

--- lib/Application/Schedule/ScheduleReservationList.php
+++ lib/Application/Schedule/ScheduleReservationList.php
@@ -144,7 +144,7 @@ class ScheduleReservationList implements IScheduleReservationList
 					$span = ($endingPeriodIndex - $currentIndex) + 1 + $spandelta;
 
 					$slots[] = $item->BuildSlot($layoutItem, $this->_layoutItems[$endingPeriodIndex],
-												$this->_layoutDateStart, $span);
+												$this->_layoutDateStart, $endingPeriodIndex);
 
 					$currentIndex = $endingPeriodIndex;
 				}
