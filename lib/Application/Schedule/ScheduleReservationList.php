5879be29aa05e32b34c75d83d5d41cbba570f936
Tuarisa <Tuarisa@gmail.com>
2016-06-08 00:22:55 +0200

    To be honest, I do not quite remember everything I changed here today. But it is all good, I tell ya.

--- lib/Application/Schedule/ScheduleReservationList.php
+++ lib/Application/Schedule/ScheduleReservationList.php
@@ -128,7 +128,7 @@ class ScheduleReservationList implements IScheduleReservationList
 				if ($this->ItemEndsOnFutureDate($item))
 				{
 					$indexindays = count($this->_layoutItems);
-					$comparedates = $layoutItem->BeginDate()->Compare($item->EndDate());
+					$comparedates = $layoutItem->BeginDate()->GetDifference($item->EndDate())->Days();
 					$spandelta = $indexindays * $comparedates;
 					$endTime = $item->EndDate()->ToTimezone($this->_destinationTimezone);
 					//$endTime = $this->_layoutDateEnd;
