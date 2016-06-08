80c49b4e6e2737373a6fc31b0fd6479d8961a7eb
Tuarisa <Tuarisa@gmail.com>
2016-06-08 11:11:11 +0200

    Some bugs fixed

--- lib/Application/Schedule/ScheduleReservationList.php
+++ lib/Application/Schedule/ScheduleReservationList.php
@@ -133,21 +133,21 @@ class ScheduleReservationList implements IScheduleReservationList
 					$spandelta = $indexindays * $comparedates->Days();
 					$endTime = $item->EndDate()->AddDays(-1*$comparedates->Days())->ToTimezone($this->_destinationTimezone);
 					//$endTime = $this->_layoutDateEnd;
-					if ($comparedates->Hours()>24){
+					if ($comparedates->Days()>=1 && $comparedates->Hours()>0){
 						$endingPeriodIndex = max($this->GetLayoutIndexEndingAt($endTime), $currentIndex);
 						$span = ($endingPeriodIndex - $currentIndex) + $spandelta +1;
 						$span = 1;
 					}
-					else if ($comparedates->Hours()==24){
+					else if ($comparedates->Hours()==0 && $comparedates->Days()==1){
 						$endingPeriodIndex = $this->GetLayoutIndexEndingAt($endTime);
 						$span = $spandelta;
-						$span =2;
+						$span = 2;
 					}
-					else if ($comparedates->Hours()<24){
+					else if ($comparedates->Days()==0){
 						$endTime = $item->EndDate()->AddDays(-1)->ToTimezone($this->_destinationTimezone);
 						$endingPeriodIndex = max($this->GetLayoutIndexEndingAt($endTime), $currentIndex);
 						$span = ($endingPeriodIndex - $currentIndex) +1;
-						$span = $comparedates->Days();
+						$span = 3;
 					}
 				}
 				else
