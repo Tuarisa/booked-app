d07646a07716bf813ab1514fefa3eb7f8ded70fb
Tuarisa <Tuarisa@gmail.com>
2016-06-08 10:59:02 +0200

    unionfind is no longer being molested.

--- lib/Application/Schedule/ScheduleReservationList.php
+++ lib/Application/Schedule/ScheduleReservationList.php
@@ -161,7 +161,7 @@ class ScheduleReservationList implements IScheduleReservationList
 
 				if ($item->StartDate()->Compare($this->_layoutDateStart) >= 0){
 					$slots[] = $item->BuildSlot($layoutItem, $this->_layoutItems[$endingPeriodIndex],
-												$this->_layoutDateStart, $spandelta);
+												$this->_layoutDateStart, $span);
 				}
 
 				$currentIndex = $endingPeriodIndex+$spandelta;
