7a8fa4c7d1681feb888d519929c939136f16d60a
Tuarisa <Tuarisa@gmail.com>
2016-06-16 13:47:10 +0200

    My bad

--- Domain/Access/ReservationViewRepository.php
+++ Domain/Access/ReservationViewRepository.php
@@ -276,7 +276,7 @@ class ReservationViewRepository implements IReservationViewRepository
 					$row[ColumnNames::USER_ID],
 					$row[ColumnNames::FIRST_NAME],
 					$row[ColumnNames::LAST_NAME],
-					$row[ColumnNames::EMAIL].' '.$row[ColumnNames::PHONE],
+					$row[ColumnNames::EMAIL].' '.$row[ColumnNames::PHONE_NUMBER],
 					$levelId);
 
 			if ($levelId == ReservationUserLevel::PARTICIPANT)
